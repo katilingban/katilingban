96be4763ed02b95adfea3b99876090a131293c0f
Ernest Guevarra <ernest@guevarra.io>
2020-07-08 22:27:35 +0100

    create datasets

---
+++ R/data.R
@@ -0,0 +1,13 @@
+################################################################################
+#
+#'
+#' Katilingban consultants' experience list
+#'
+#' @format A tibble with 106 rows and 12 columns
+#'
+#' @examples
+#' kb_experience
+#'
+#
+################################################################################
+"kb_experience"
