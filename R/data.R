8a1cc420cda21ab78d35b9106ccb670840bf8c0a
Ernest Guevarra <ernest@guevarra.io>
2020-07-09 10:42:49 +0100

    update datasets

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #'
 #' Katilingban consultants' experience list
 #'
-#' @format A tibble with 106 rows and 12 columns
+#' @format A tibble with 141 rows and 12 columns
 #'
 #' @examples
 #' kb_experience
